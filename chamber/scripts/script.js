636eb2ae471a10fe24fe0abf86462cf4c9b68f8a
tywill3s <114180280+tywill3s@users.noreply.github.com>
2023-01-30 13:06:08 -0700

    Chamber Updates 2

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,36 +1,15 @@
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
+function updateTime() {
 
-document.querySelector("#year-field").innerText = currentYear;
-document.querySelector("#last-update-field").innerText = document.lastModified;
-document.querySelector("#current-date").textContent = Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(currentDate);
+  var timeElement = document.getElementById("last-update");
 
+  var lastModified = document.lastModified;
 
-const buttonOpen = document.getElementById("burger-open");
-const buttonClose = document.getElementById("burger-close");
-function toggleMenu() {
-    console.log("Click-eth-ed");
-    document.getElementById("burger-nav").classList.toggle("hidden");
-    
-    if(document.getElementById("burger-nav").classList.contains("hidden")) {
-        buttonOpen.classList.remove("hidden");
-        buttonClose.classList.add("hidden"); }
-    else {
-        buttonOpen.classList.add("hidden");
-        buttonClose.classList.remove("hidden");  }
+  timeElement.textContent = "Last update: " + lastModified;
 }
-
-document.getElementById("burger-open").onclick = toggleMenu;
-document.getElementById("burger-close").onclick = toggleMenu;
+window.onload = updateTime;
 
 
-/* const body = document.querySelector("header");
-const observer = new IntersectionObserver( 
-  ([event]) => { event.target.childNodes[1].classList.toggle("menu-pinned", event.intersectionRatio < 1); console.log(event.intersectionRatio)},
-  { threshold: [1] }
-);
-//todo: I think the intersection is not being met.  header-gradient is never allowed to intersect?
-
-observer.observe(body); */
+let currentDate = new Date();
+let options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+let dateString = currentDate.toLocaleDateString('en-US', options);
+document.querySelector('#currentDate').textContent = dateString;
