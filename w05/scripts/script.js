aa3b4f5e61a8e630e20fc1c77afbfd6c18c43a54
tywill3s <114180280+tywill3s@users.noreply.github.com>
2023-02-02 12:46:47 -0700

    DOM UPDATES

--- w05/scripts/script.js
+++ w05/scripts/script.js
@@ -1,10 +1,17 @@
 const input = document.getElementById("favchap");
 const button = document.getElementById("button");
 const list = document.getElementById("list");
+console.log(list);
 
 function generate() {
     const value = document.getElementById("favchap").value;
     if (value != "") {
-        console.log("works");
+        var li = document.createElement("li");
+        var delButton = document.createElement("button");
+        li.innerHTML = value;
+        delButton.textContent = "❌";
+        li.append(delButton);
+        list.append(li);
+        console.log(li)
     }
 }
