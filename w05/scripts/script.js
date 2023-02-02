e6a2ea4832ef160869ec25a84e7bc95697626db9
tywill3s <114180280+tywill3s@users.noreply.github.com>
2023-02-02 12:19:03 -0700

    DOM UPDATE

---
+++ w05/scripts/script.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("favchap");
+const button = document.getElementById("button");
+const list = document.getElementById("list");
+
+function generate() {
+    const value = document.getElementById("favchap").value;
+    if (value != "") {
+        console.log("works");
+    }
+}
