934c303687ccffe09ebc84971bede51d32988fd1
tywill3s <114180280+tywill3s@users.noreply.github.com>
2023-02-02 15:44:28 -0700

    DOM UPDATE

--- w05/scripts/script.js
+++ w05/scripts/script.js
@@ -1,16 +1,23 @@
 const input = document.getElementById("favchap");
 const button = document.getElementById("button");
-const list = document.getElementById('list');
+const list = document.getElementById("list");
 console.log(list);
 
 function generate() {
-    const value = document.getElementById("favchap").value;
+    var value = document.getElementById("favchap").value;
     if (value != "") {
         var li = document.createElement("li");
         var delButton = document.createElement("button");
-        li.innerHTML = value;
+        var txtNode = document.createTextNode(value);
+        li.appendChild(txtNode);
         delButton.textContent = "❌";
+        delButton.addEventListener('click',function() {
+            li.innerHTML = "";
+        })
         li.append(delButton);
-        console.log(li)
+        console.log(li);
+        list.appendChild(li);
+        input.value.textContent = "";
+        value = "";
     }
 }
