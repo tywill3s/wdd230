f2ddc327c6e8f18304aa2dbf9e463a1a576605b0
tywill3s <114180280+tywill3s@users.noreply.github.com>
2023-02-09 16:15:52 -0700

    W04 Professor Updates

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,39 +1,52 @@
-function updateTime() {
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
 
-  var timeElement = document.getElementById("last-update");
+document.querySelector("#year-field").innerText = currentYear;
+document.querySelector("#last-update-field").innerText = document.lastModified;
+document.querySelector("#current-date").textContent = Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(currentDate);
 
-  var lastModified = document.lastModified;
 
-  timeElement.textContent = "Last update: " + lastModified;
+const buttonOpen = document.getElementById("burger-open");
+const buttonClose = document.getElementById("burger-close");
+const burgerMenu = document.getElementById("burger-menu");
+
+function toggleMenu() {
+    burgerMenu.classList.toggle("hidden");
+    
+    if(burgerMenu.classList.contains("hidden")) {
+        burgerMenu.classList.remove("flex");
+        
+        buttonOpen.classList.remove("hidden");
+        buttonClose.classList.add("hidden"); }
+    else {
+        burgerMenu.classList.add("flex");
+
+        buttonOpen.classList.add("hidden");
+        buttonClose.classList.remove("hidden");  }
 }
-window.onload = updateTime;
+
+document.getElementById("burger-open").onclick = toggleMenu;
+document.getElementById("burger-close").onclick = toggleMenu;
 
 
-let currentDate = new Date();
-let options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
-let dateString = currentDate.toLocaleDateString('en-US', options);
-document.querySelector('#currentDate').textContent = dateString;
+const day = new Date().getDay();
+const header = document.querySelector("header");
 
+const dayOfTheWeek = [
+    "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+];
 
-// Get the current date
-var today = new Date();
-
-// Get the current day of the week (0 for Sunday, 1 for Monday, etc.)
-var dayOfWeek = today.getDay();
-
-// Check if today is Monday or Tuesday
-if (dayOfWeek === 1 || dayOfWeek === 2) {
-  // Create the banner element
-  var banner = document.createElement("div");
-  banner.style.position = "fixed";
-  banner.style.top = 0;
-  banner.style.width = "98.65em";
-  banner.style.backgroundColor = "#fff";
-  banner.style.padding = "10px";
-  banner.style.textAlign = "center";
-  banner.innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-  banner.style.zIndex = 1;
-  banner.id = "banner";
-  // Add the banner to the page
-  document.body.prepend(banner);
+if(dayOfTheWeek[day] == "Monday" | dayOfTheWeek[day] == "Tuesday") {
+    const alertBanner = document.createElement("div");
+    alertBanner.innerHTML = '<span>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</span>';
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<img src="images/close-icon.svg" alt="close alert" width="20" height="20">'
+    alertBanner.appendChild(deleteButton);
+    alertBanner.classList.add("alert");
+    header.prepend(alertBanner);
+    deleteButton.addEventListener("click", () => {
+        alertBanner.remove();    
+});
 }
